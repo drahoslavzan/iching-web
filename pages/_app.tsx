4897fd20f5acbd2fd19797c770bbbb5ab1231f7d
Drahoslav Zan <zandrahoslav@gmail.com>
2020-05-11 00:00:43 +0200

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,9 +13,10 @@ export default function App(props: AppProps) {
             <Head>
                 <title>{config.app}</title>
                 <meta
-                name="viewport"
-                content="minimum-scale=1, initial-scale=1, width=device-width"
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
                 />
+                <link rel="shortcut icon" href="/public/favicon.ico" />
             </Head>
             <Component {...pageProps} />
         </AnalyticsContext.Provider>
