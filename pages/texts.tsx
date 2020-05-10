f34ac465dae45e88e8697c65d5a8be156d09b993
Drahoslav Zan <zandrahoslav@gmail.com>
2020-05-10 23:38:53 +0200

    links

--- pages/texts.tsx
+++ pages/texts.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { config } from '../src/config';
 
 export default function Texts() {
@@ -6,6 +7,9 @@ export default function Texts() {
         <div className="container mx-auto text-center w-full h-full mt-10">
             <h1 className="text-4xl mb-8">Under Constraction</h1>
             <p>If you have any questions, do not hesitate to contact us on <a className="font-bold" href={`mailto:${config.email}`}>{config.email}</a></p>
+            <div className="mt-10">
+                Continue to <Link href="/"><a className="font-bold">Home</a></Link>
+            </div>
         </div>
     );
 }
